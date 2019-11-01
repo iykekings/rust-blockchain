4ddc313c6228a967064bf3d9ab5fe5b5ac77f390
mattshardman <mattshardman@gmail.com>
2019-11-01 16:38:18 +0000

    create BlockChain struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let b = BlockChain {
+        pending_transactions: vec![1, 2],
+        chain: vec![1, 2]
+    };
+    
+    println!("{:#?}", b);
+}
+
+#[derive(Debug)]
+struct BlockChain {
+    pending_transactions: Vec<u32>, // will be Vec<Transaction>
+    chain: Vec<u32> // will be Vec<Block>
 }
