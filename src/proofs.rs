109ac2aceb7123de2fadfacfb72a0ce1af46019b
mattshardman <mattshardman@gmail.com>
2019-11-14 14:27:16 +0800

    create generate_proof function

--- src/proofs.rs
+++ src/proofs.rs
@@ -4,3 +4,15 @@ pub fn confirm_proof(hash: &String) -> bool {
     &hash[..5] == "00000"
 }
 
+pub fn generate_proof(chain: &BlockChain) -> u32 {
+    let mut proof = 0;
+    let mut confirmed_hash = false;
+    while confirmed_hash != true {
+        proof += 1;
+        let temp_hash = chain.hash(proof);
+        if confirm_proof(&temp_hash) == true {
+            confirmed_hash = true;
+        }
+    }
+    proof
+}
