b2e5ec0f33d0849ddc4fe33ed63def0b3f861d95
mattshardman <mattshardman@gmail.com>
2019-11-14 14:27:42 +0800

    check functions

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
-fn main() {
-    let b = BlockChain {
-        pending_transactions: vec![1, 2],
-        chain: vec![1, 2]
-    };
-    
-    println!("{:#?}", b);
-}
+mod blockchain;
+mod proofs;
 
-#[derive(Debug)]
-struct BlockChain {
-    pending_transactions: Vec<u32>, // will be Vec<Transaction>
-    chain: Vec<u32> // will be Vec<Block>
+use crate::blockchain::{ BlockChain, Transaction };
+use crate::proofs::generate_proof;
+
+fn main() {
+    let mut b = BlockChain::create_genesis();
+    let new_transaction = Transaction::create("me".to_string(), "you".to_string(), 10);
+    b.add_transaction(new_transaction);
+    println!("{:#?}", b);
+    let proof = generate_proof(&b);
+    b.add_block(235);
+    println!("{:#?}", b);
 }
