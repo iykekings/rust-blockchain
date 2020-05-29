aac4ca7749a124fedfe1a3e44cdc5da4bf86e596
Ikechukwu Eze <iykekings36@gmail.com>
2020-05-29 02:05:19 +0100

    refactor: generate_proof to use iterator

--- src/proofs.rs
+++ src/proofs.rs
@@ -1,18 +1,11 @@
 use crate::blockchain::BlockChain;
 
 pub fn confirm_proof(hash: &String) -> bool {
-    &hash[..5] == "00000"
+    &hash[..4] == "0000"
 }
 
 pub fn generate_proof(chain: &BlockChain) -> u32 {
-    let mut proof = 0;
-    let mut confirmed_hash = false;
-    while confirmed_hash != true {
-        proof += 1;
-        let temp_hash = chain.hash(proof);
-        if confirm_proof(&temp_hash) == true {
-            confirmed_hash = true;
-        }
-    }
-    proof
+    (0..)
+    .skip_while(|&x| !confirm_proof(&chain.hash(x)))
+    .next().unwrap() as u32
 }
