ad64a0bcb673790be11c295dabd7171795b1dfe7
iykekings <iykekings36@gmail.com>
2020-05-29 10:12:29 +0100

    update to fork branch

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
+use std::time::Instant;
 mod blockchain;
 mod proofs;
 
-use crate::blockchain::{ BlockChain, Transaction };
+use crate::blockchain::{BlockChain, Transaction};
 use crate::proofs::generate_proof;
 
 fn main() {
+    let now = Instant::now();
     let mut b = BlockChain::create_genesis();
     let new_transaction = Transaction::create("me".to_string(), "you".to_string(), 10);
     b.add_transaction(new_transaction);
@@ -12,4 +14,5 @@ fn main() {
     let proof = generate_proof(&b);
     b.add_block(proof);
     println!("{:#?}", b);
+    println!("Took: {:?}", now.elapsed());
 }
--- src/proofs.rs
+++ src/proofs.rs
@@ -1,11 +1,12 @@
 use crate::blockchain::BlockChain;
 
 pub fn confirm_proof(hash: &String) -> bool {
-    &hash[..4] == "0000"
+    &hash[..5] == "00000"
 }
 
 pub fn generate_proof(chain: &BlockChain) -> u32 {
     (0..)
-    .skip_while(|&x| !confirm_proof(&chain.hash(x)))
-    .next().unwrap() as u32
+        .skip_while(|&x| !confirm_proof(&chain.hash(x)))
+        .next()
+        .unwrap() as u32
 }
